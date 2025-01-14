132ac34a2eaab8fc81a7a4468e32ce6c609c52d4
Shattuck Kuzmic <shattuckkuzmic@gmail.com>
2025-01-14 13:49:01 -0700

    Update

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from wtforms.validators import DataRequired
 import requests
 
 
-MOVIE_DB_API_KEY = "a90c8a7fd6ca9e772e5ae13ca7db7987"
+MOVIE_DB_API_KEY = "a90c8a7fd6ca9e772e5ae13ca7db79873"
 MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
 MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
 MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
